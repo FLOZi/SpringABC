2a13f0c86fb06482f11239efe3929f006f1f582c
Craig Lawrence <flozi@blueyonder.co.uk>
2014-05-15 19:51:07 +0100

    First commit

---
+++ A.sdd/ModInfo.lua
@@ -0,0 +1,16 @@
+local modinfo = {
+	name = "ABC - A(lmost Minimal)",
+	shortname = "A",
+	game = "SpringABC",
+	shortgame = "ABC",
+	description = "Game with empty directory structure",
+	url = "http://www.springrts.com/wiki/SpringABC",
+	
+	modtype = "1",
+
+	depend = {
+		"cursors.sdz",
+	}
+}
+
+return modinfo
---
+++ B.sdd/ModInfo.lua
@@ -0,0 +1,16 @@
+local modinfo = {
+	name = "ABC - B(are Bones)",
+	shortname = "B",
+	game = "SpringABC",
+	shortgame = "ABC",
+	description = "Game with directory structure + major files",
+	url = "http://www.springrts.com/wiki/SpringABC",
+	
+	modtype = "1",
+
+	depend = {
+		"cursors.sdz",
+	}
+}
+
+return modinfo
